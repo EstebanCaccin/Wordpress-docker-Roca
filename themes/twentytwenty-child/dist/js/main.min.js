cabb9ac41f7a1c165cd8a43dffa83c1b4911cfee
Esteban Caccin <esteban.caccin@4rsoluciones.com>
2021-07-07 15:10:26 -0300

    Dist

---
+++ themes/twentytwenty-child/dist/js/main.min.js
@@ -0,0 +1,9 @@
+jQuery(function ($) {
+  $(document).ready(function () {
+    console.log('JS READY 2');
+  });
+
+  $(window).load(function () {});
+
+  $(window).on("resize", function () {});
+});
